612eec3a64238c145a6aded764e5ef35ec0c23d7
DuCanhGH <ngoducanh2912@gmail.com>
2020-12-20 12:13:49 +0700

    first code

---
+++ preventcovid.js
@@ -0,0 +1,24 @@
+function display(){
+    document.getElementById("text1").style.display="block"
+}
+function hide(){
+    document.getElementById("text1").style.display="none"
+}
+function display1(){
+    document.getElementById("text3").style.display="block"
+}
+function hide1(){
+    document.getElementById("text3").style.display="none"
+}
+function display2(){
+    document.getElementById("text5").style.display="block"
+}
+function hide2(){
+    document.getElementById("text5").style.display="none"
+}
+function display3(){
+    document.getElementById("text6").style.display="block"
+}
+function hide3(){
+    document.getElementById("text6").style.display="none"
+}
